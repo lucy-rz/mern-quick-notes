3bc2d6f989eec9c3642d4eb113c54666f5bd7873
Lucy Ruiz <lucyruizwa@gmail.com>
2024-05-14 19:40:55 -0700

    first commit

---
+++ controllers/api/users.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    create,
+    login,
+};
+
+async function login(req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) throw new Error();
+        console.log(user)
+        const match = await bcrypt.compare(req.body.password, user.password);
+        console.log(match)
+        if (!match) throw new Error();
+        res.json( createJWT(user) );
+      } catch {
+        res.status(400).json('Bad Credentials');
+      }
+}
+
+async function create(req, res){
+    try {
+        console.log('test2')
+        const user = await User.create(req.body);
+        console.log(user)
+        const token = createJWT(user);
+        console.log(token)
+        
+        res.json(token);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+};
+
+//helper functions
+function createJWT(user) {
+    return jwt.sign(
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    );
+}
